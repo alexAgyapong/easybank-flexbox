44e55d7cf8f89460a2aaf65c9be3927259e766eb
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-04-30 23:10:13 +0100

    Mobile design complete. Desktop version about 90% complete.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EasyBankComponent } from './easy-bank/easy-bank.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: '', component: EasyBankComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/easy-bank/easy-bank.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-easy-bank',
+  templateUrl: './easy-bank.component.html',
+  styleUrls: ['./easy-bank.component.css']
+})
+export class EasyBankComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
